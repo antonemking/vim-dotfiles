325a9d7f748d7e45f8855e72d9f992002fd21ead
Antone king <toneking972@gmail.com>
2022-08-10 17:58:11 -0400

    Update init.lua

--- after/plugin/keymap/init.lua
+++ after/plugin/keymap/init.lua
@@ -2,3 +2,6 @@ local Remap = require("theking.keymap")
 local nnoremap = Remap.nnoremap
 
 nnoremap("<leader>pv", ":Ex<CR>")
+nnoremap("<leader>w", ":w<CR>")
+nnoremap("<leader>zk", ":ZettelNew<CR>")
+nnoremap("<leader>gm", ":LivedownToggle<CR>")
