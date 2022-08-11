11639b31559f8665056bff982b6a93727bc39770
Antone king <toneking972@gmail.com>
2022-08-11 01:51:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 
+# stow dotfiles
+# stow -R nvim
+# stow -R zsh
+
+# add zsh as a login shell
+# command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+# sudo chsh -s $(which zsh) $USER
